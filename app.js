a81b5e660f5aa3fe5ab0ffcf985fb63d480537e7
Sheikh Hafsa Nadeem <124384550+hafsa-nadim@users.noreply.github.com>
2023-05-16 00:07:06 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,80 @@
+// //     Question no:1
+
+// var a = +prompt("enter a number");
+// document.write("the value of a is  :  " , a , "<br>");
+// document.write("------------------------------<br>");
+// document.write("the value of ++a is  :  " , ++a , "<br>");
+// document.write("now the value of a is :  " , a , "<br><br><br>");
+
+// document.write("the value of a++ is :  " , a++ , "<br>");
+// document.write("now the value of a is :  " , a , "<br><br><br>");
+
+// document.write("the value of a-- is :  " , a-- , "<br>");
+// document.write("now the value of a is :  " , a , "<br>");
+
+
+
+
+// //   Question no:2
+// var a = +prompt("enter number 1");
+// var b = +prompt("enter number 2");
+// document.write("value of a is : " , a , "<br>");
+// document.write("value of b is : " , b , "<br>");
+// document.write("solving the expression for --a - --b + ++b + b--  <br><br>")
+// var step1 = --a;
+// var step2 = --b;
+// var step3 = ++b;
+// var step4 = b--;
+
+// document.write("--a is : ", step1  , "<br>");
+// document.write("--b is : ", step2  , "<br>");
+// document.write("++b is : ", step3  , "<br>");
+// document.write("b-- is : ", step4 , "<br>");
+
+// document.write("result for --a - --b + ++b + b-- is : ", step_1-step_2+step_3+step_4);
+
+
+
+
+
+// //   Question no:3
+// var myName = prompt("enter your name ");
+// document.write("welcome " + myName);
+
+
+// //   Question no:4
+
+// var number = prompt("Enter a number:")
+// if (!number) {
+//     for (i = 0; i <= 10; i++) {
+//         document.write(5, " X ", i, " = ", 5 * i, " <br>");
+//     }
+// }
+// else{
+// number = +number;
+// for (i = 0; i <= 10; i++) {
+//     document.write(number, " X ", i, " = ", number * i, "<br>");
+// }
+// }
+
+
+//      Question no:5
+
+// var name1 = prompt("enter name of subject 1");
+// var name2 = prompt("enter name of subject 2");
+// var name3 = prompt("enter name of subject 3");
+// var marks1 = +prompt("enter total marks of : ",name1) 
+// var marks2 = +prompt("enter total marks of : ",name2) 
+// var marks3 = +prompt("enter total marks of : " ,name3) 
+// var obtMarks1 = +prompt("enter obtained marks of : ", name1)
+// var obtMarks2 = +prompt("enter obtained marks of : ", name2)
+// var obtMarks3 = +prompt("enter obtained marks of : ", name3)
+// var createTable = document.createElement("table")
+// var table = document.getElementsByTagName(table);
+// var row = table.insertRow(0);
+//   var cell1 = row.insertCell(0);
+//   var cell2 = row.insertCell(1);
+//   var cell3 = row.insertCell(2);
+
+//   cell1.innerHTML = "english";
+//   cell2.innerHTML = "urdu";
